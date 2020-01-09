bb190da54492ccabdfdb82eb133dbb94a08d18b8
rwan <erwan_lemaire@yahoo.fr>
2020-01-09 09:10:35 +0100

    first commit after looses

---
+++ scripts/createmonsters.js
@@ -0,0 +1,580 @@
+Hooks.once("init", async function() {
+    let template1 = {
+        "_id": "jBeWn9Ly3sSAWUUS",
+        "name": "ezrzr",
+        "type": "character",
+        "data": {
+            "abilities": {
+                "str": {
+                    "value": 10,
+                    "min": 3,
+                    "proficient": 0,
+                    "mod": 0,
+                    "save": 0
+                },
+                "dex": {
+                    "value": 10,
+                    "min": 3,
+                    "proficient": 0,
+                    "mod": 0,
+                    "save": 0
+                },
+                "con": {
+                    "value": 10,
+                    "min": 3,
+                    "proficient": 0,
+                    "mod": 0,
+                    "save": 0
+                },
+                "int": {
+                    "value": 10,
+                    "min": 3,
+                    "proficient": 0,
+                    "mod": 0,
+                    "save": 0
+                },
+                "wis": {
+                    "value": 10,
+                    "min": 3,
+                    "proficient": 0,
+                    "mod": 0,
+                    "save": 0
+                },
+                "cha": {
+                    "value": 10,
+                    "min": 3,
+                    "proficient": 0,
+                    "mod": 0,
+                    "save": 0
+                }
+            },
+            "attributes": {
+                "ac": {
+                    "min": 0
+                },
+                "hp": {
+                    "value": 10,
+                    "min": 0,
+                    "max": 10,
+                    "temp": 0,
+                    "tempmax": 0
+                },
+                "init": {
+                    "value": 0,
+                    "bonus": 0,
+                    "mod": 0,
+                    "prof": 0,
+                    "total": 0
+                },
+                "prof": 2,
+                "speed": {
+                    "value": "30 ft",
+                    "special": ""
+                },
+                "spellcasting": "int",
+                "spelldc": 10,
+                "hd": {
+                    "value": 1,
+                    "max": 0
+                },
+                "death": {
+                    "success": 0,
+                    "failure": 0
+                },
+                "exhaustion": 0,
+                "inspiration": 0
+            },
+            "details": {
+                "alignment": "",
+                "biography": {
+                    "value": "",
+                    "public": ""
+                },
+                "race": "",
+                "background": "",
+                "level": {
+                    "value": 1,
+                    "min": 1,
+                    "max": 20
+                },
+                "xp": {
+                    "value": 0,
+                    "min": 0,
+                    "max": 300,
+                    "pct": 0
+                },
+                "trait": "",
+                "ideal": "",
+                "bond": "",
+                "flaw": ""
+            },
+            "skills": {
+                "acr": {
+                    "value": 0,
+                    "ability": "dex",
+                    "bonus": 0,
+                    "mod": 0,
+                    "passive": 10
+                },
+                "ani": {
+                    "value": 0,
+                    "ability": "wis",
+                    "bonus": 0,
+                    "mod": 0,
+                    "passive": 10
+                },
+                "arc": {
+                    "value": 0,
+                    "ability": "int",
+                    "bonus": 0,
+                    "mod": 0,
+                    "passive": 10
+                },
+                "ath": {
+                    "value": 0,
+                    "ability": "str",
+                    "bonus": 0,
+                    "mod": 0,
+                    "passive": 10
+                },
+                "dec": {
+                    "value": 0,
+                    "ability": "cha",
+                    "bonus": 0,
+                    "mod": 0,
+                    "passive": 10
+                },
+                "his": {
+                    "value": 0,
+                    "ability": "int",
+                    "bonus": 0,
+                    "mod": 0,
+                    "passive": 10
+                },
+                "ins": {
+                    "value": 0,
+                    "ability": "wis",
+                    "bonus": 0,
+                    "mod": 0,
+                    "passive": 10
+                },
+                "itm": {
+                    "value": 0,
+                    "ability": "cha",
+                    "bonus": 0,
+                    "mod": 0,
+                    "passive": 10
+                },
+                "inv": {
+                    "value": 0,
+                    "ability": "int",
+                    "bonus": 0,
+                    "mod": 0,
+                    "passive": 10
+                },
+                "med": {
+                    "value": 0,
+                    "ability": "wis",
+                    "bonus": 0,
+                    "mod": 0,
+                    "passive": 10
+                },
+                "nat": {
+                    "value": 0,
+                    "ability": "int",
+                    "bonus": 0,
+                    "mod": 0,
+                    "passive": 10
+                },
+                "prc": {
+                    "value": 0,
+                    "ability": "wis",
+                    "bonus": 0,
+                    "mod": 0,
+                    "passive": 10
+                },
+                "prf": {
+                    "value": 0,
+                    "ability": "cha",
+                    "bonus": 0,
+                    "mod": 0,
+                    "passive": 10
+                },
+                "per": {
+                    "value": 0,
+                    "ability": "cha",
+                    "bonus": 0,
+                    "mod": 0,
+                    "passive": 10
+                },
+                "rel": {
+                    "value": 0,
+                    "ability": "int",
+                    "bonus": 0,
+                    "mod": 0,
+                    "passive": 10
+                },
+                "slt": {
+                    "value": 0,
+                    "ability": "dex",
+                    "bonus": 0,
+                    "mod": 0,
+                    "passive": 10
+                },
+                "ste": {
+                    "value": 0,
+                    "ability": "dex",
+                    "bonus": 0,
+                    "mod": 0,
+                    "passive": 10
+                },
+                "sur": {
+                    "value": 0,
+                    "ability": "wis",
+                    "bonus": 0,
+                    "mod": 0,
+                    "passive": 10
+                }
+            },
+            "traits": {
+                "size": "med",
+                "senses": "",
+                "languages": {
+                    "value": [
+                        "draconic",
+                        "giant",
+                        "gnomish"
+                    ],
+                    "custom": "qsdfqsdfqsf"
+                },
+                "di": {
+                    "value": [],
+                    "custom": ""
+                },
+                "dr": {
+                    "value": [],
+                    "custom": ""
+                },
+                "dv": {
+                    "value": [],
+                    "custom": ""
+                },
+                "ci": {
+                    "value": [],
+                    "custom": ""
+                },
+                "perception": {
+                    "_deprecated": true
+                },
+                "weaponProf": {
+                    "value": [],
+                    "custom": ""
+                },
+                "armorProf": {
+                    "value": [],
+                    "custom": ""
+                },
+                "toolProf": {
+                    "value": [],
+                    "custom": ""
+                }
+            },
+            "currency": {
+                "pp": 0,
+                "gp": 0,
+                "ep": 0,
+                "sp": 0,
+                "cp": 0
+            },
+            "spells": {
+                "spell0": {
+                    "value": 0,
+                    "max": 0
+                },
+                "spell1": {
+                    "value": 0,
+                    "max": 0
+                },
+                "spell2": {
+                    "value": 0,
+                    "max": 0
+                },
+                "spell3": {
+                    "value": 0,
+                    "max": 0
+                },
+                "spell4": {
+                    "value": 0,
+                    "max": 0
+                },
+                "spell5": {
+                    "value": 0,
+                    "max": 0
+                },
+                "spell6": {
+                    "value": 0,
+                    "max": 0
+                },
+                "spell7": {
+                    "value": 0,
+                    "max": 0
+                },
+                "spell8": {
+                    "value": 0,
+                    "max": 0
+                },
+                "spell9": {
+                    "value": 0,
+                    "max": 0
+                }
+            },
+            "resources": {
+                "primary": {
+                    "value": 0,
+                    "max": 0,
+                    "sr": 0,
+                    "lr": 0
+                },
+                "secondary": {
+                    "value": 0,
+                    "max": 0,
+                    "sr": 0,
+                    "lr": 0
+                },
+                "tertiary": {
+                    "value": 0,
+                    "max": 0,
+                    "sr": 0,
+                    "lr": 0
+                }
+            }
+        },
+        "sort": 100001,
+        "flags": {},
+        "token": {
+            "flags": {},
+            "displayName": 0,
+            "img": "icons/svg/mystery-man.svg",
+            "width": 1,
+            "height": 1,
+            "scale": 1,
+            "elevation": 0,
+            "lockRotation": false,
+            "rotation": 0,
+            "vision": true,
+            "dimSight": 30,
+            "brightSight": 0,
+            "dimLight": 0,
+            "brightLight": 0,
+            "sightAngle": 360,
+            "lightAngle": 360,
+            "actorId": "jBeWn9Ly3sSAWUUS",
+            "actorLink": true,
+            "actorData": {},
+            "disposition": 1,
+            "displayBars": 0,
+            "bar1": {},
+            "bar2": {},
+            "randomImg": false
+        },
+        "items": [],
+        "img": "icons/svg/mystery-man.svg"
+    };
+
+
+    let bestiaire = {};
+
+    $.getJSON("/modules/monstres fr HD/bestiaire.json", function(data) {
+
+        bestiaire = data;
+        console.log(bestiaire);
+
+        function MergeRecursive(obj1, obj2) {
+
+            for (var p in obj2) {
+                try {
+                    // Property in destination object set; update its value.
+                    if (obj2[p].constructor == Object) {
+                        obj1[p] = MergeRecursive(obj1[p], obj2[p]);
+                    } else {
+                        obj1[p] = obj2[p];
+                    }
+                } catch (e) {
+                    // Property in destination object not set; create it and set its value.
+                    obj1[p] = obj2[p];
+                }
+            }
+            return obj1;
+        };
+
+
+
+
+
+
+        for (let element of bestiaire) {
+
+
+            element = MergeRecursive(element, template1);
+
+
+
+            setProperty(element, "name", element.header.title);
+            element.data.abilities.str.value = parseInt(element.header.monster.str);
+            element.data.abilities.dex.value = parseInt(element.header.monster.dex);
+            element.data.abilities.con.value = parseInt(element.header.monster.con);
+            element.data.abilities.int.value = parseInt(element.header.monster.int);
+            element.data.abilities.wis.value = parseInt(element.header.monster.wis);
+            element.data.abilities.cha.value = parseInt(element.header.monster.cha);
+
+
+            element.data.abilities.str.mod = element.header.monster.str.split("(")[1];
+            element.data.abilities.str.mod = element.data.abilities.str.mod.substring(0, element.data.abilities.str.mod.length - 1);
+
+            element.data.abilities.dex.mod = element.header.monster.dex.split("(")[1];
+            element.data.abilities.dex.mod = element.data.abilities.dex.mod.substring(0, element.data.abilities.dex.mod.length - 1);
+
+            element.data.abilities.con.mod = element.header.monster.con.split("(")[1];
+            element.data.abilities.con.mod = element.data.abilities.con.mod.substring(0, element.data.abilities.con.mod.length - 1);
+
+            element.data.abilities.int.mod = element.header.monster.int.split("(")[1];
+            element.data.abilities.int.mod = element.data.abilities.int.mod.substring(0, element.data.abilities.int.mod.length - 1);
+
+            element.data.abilities.wis.mod = element.header.monster.wis.split("(")[1];
+            element.data.abilities.wis.mod = element.data.abilities.wis.mod.substring(0, element.data.abilities.wis.mod.length - 1);
+
+            element.data.abilities.cha.mod = element.header.monster.cha.split("(")[1];
+            element.data.abilities.cha.mod = element.data.abilities.cha.mod.substring(0, element.data.abilities.cha.mod.length - 1);
+
+
+            element.data.details.cr = element.header.monster.challenge;
+            element.data.details.alignment = element.header.monster.alignment;
+            element.data.details.xp.value = element.header.monster.px;
+            element.data.details.biography.value = element.content;
+            element.data.details.type = element.truetype;
+            element.data.details.environment = element.environment;
+            element.data.details.source = element.header.taxonomy.source;
+
+            element.data.attributes.ac.value = parseInt(element.header.monster.ac);
+            element.data.attributes.hp.value = parseInt(element.header.monster.hp);
+            element.data.attributes.hp.max = element.data.attributes.hp.value;
+            element.data.attributes.hp.formula = element.header.monster.hp.split("(")[1];
+            element.data.attributes.hp.formula = "0";
+            element.data.attributes.speed.value = element.header.monster.speed;
+
+            //resultat aléatoire.... mais ça rempli quelques images tout de même
+            path = 'https://raw.githubusercontent.com/Nioux/AideDeJeu/master/Data/Monsters/' + element.name.toLowerCase() + '.jpg';
+            element.img = path;
+            element.data.traits.languages.custom = element.header.monster.languages;
+
+
+            element.data.traits.senses = element.header.monster.senses;
+
+            switch (element.header.monster.size) {
+                case 'TP':
+                    element.data.traits.size = 'tiny'
+                    break;
+                case 'P':
+                    element.data.traits.size = 'sm'
+                    break;
+                case 'M':
+                    element.data.traits.size = 'med'
+                    break;
+                case 'G':
+                    element.data.traits.size = 'lg'
+                    break;
+                case 'TG':
+                    element.data.traits.size = 'huge'
+                    break;
+                case 'Gig':
+                    element.data.traits.size = 'grg'
+                    break;
+            };
+
+            if (element.header.monster.hasOwnProperty('immunities')) {
+                element.data.traits.di.custom = element.header.monster.immunities;
+            } else {
+                element.data.traits.di.custom = "";
+            }
+            if (element.header.monster.hasOwnProperty('resistances')) {
+                element.data.traits.dr.custom = element.header.monster.resistances;
+            } else {
+                element.data.traits.dr.custom = "";
+            }
+
+            element.data.traits.dv.custom = element.header.monster.vulnerabilities;
+            if (element.header.monster.hasOwnProperty('vulnerabilities')) {
+                element.data.traits.dv.custom = element.header.monster.vulnerabilities;
+            } else {
+                element.data.traits.dv.custom = "";
+            }
+            if (element.header.monster.hasOwnProperty('immunities_debilities')) {
+                element.data.traits.ci.custom = element.header.monster.immunities_debilities;
+            } else {
+                element.data.traits.ci.custom = "";
+            }
+
+            console.log('----------------------' + element.name + '------------------------------------------')
+            console.log(element);
+            var delayInMilliseconds = 30;
+
+
+        }
+
+
+
+        console.log(bestiaire);
+
+    });
+
+    console.log(`--------Heros et Dragons SRD Ready`);
+    console.log(`
+       .......................................
+
+       .....................:*=#=+-...........
+       
+       ..................-=##=-...............
+       
+       ................-=####*:*=#######*:....
+       
+       ...........-:..*#############*:........
+       
+       ..........=#####################=*-....
+       
+       .........*##########################=-.
+       
+       ........-########################*-::+-
+       
+       ...-...+###########=-..+##########=-...
+       
+       ..+#:+=#######:=##:.....-###########=..
+       
+       .-##########=-:##+......+########+.*#+.
+       
+       .+#####=+....-=#=......*########=....:.
+       
+       .:#=+-..-*##====:....+##########*......
+       
+       ..-..-++:-........-=###########=.......
+       
+       ...............-=#############=........
+       
+       .............-=#############*..........
+       
+       ............+##########=+-.............
+       
+       ............######=+-..................
+       
+       ............=##=:......................
+       
+       .............+.........................
+       `);
+
+    //--------------------------------change logo
+    var logo = document.getElementById("logo");
+    logo.setAttribute("src", "modules/monstres fr HD/img/logoHD.png");
+    document.body.childNodes.className = +"owl-carousel";
+});
+Hooks.on("renderChatMessage", function() {
+
+});
