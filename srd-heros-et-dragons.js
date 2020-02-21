4ee2e24691b1b79e9ed6f8234c5d54d700cbb974
rwan <erwan_lemaire@yahoo.fr>
2020-02-21 11:18:03 +0100

    ça a l'air de marcher

--- srd-heros-et-dragons.js
+++ srd-heros-et-dragons.js
@@ -1,6 +1,11 @@
 /*
 _________________création des objets javascript une fois intit
 */
+
+
+
+
+
 import { packMonsters } from './modules/monsters.js';
 import { prepareCreatureData } from './modules/monsters.js';
 import { packSpells } from './modules/spells.js';
@@ -13,10 +18,6 @@ Hooks.once("init", async function() {
 
 
 });
-
-
-
-
 
 async function monsterCreation() {
 
@@ -31,7 +32,7 @@ async function monsterCreation() {
 /*-------tests en cours-------------*/
 
 
-async function spellsCreation() {
+async function spellCreation() {
     let srdSpell = await Compendium.create({ entity: "Item", label: "grimoire_DRS_H&D" });
 
     for (var spell of packSpells) {
@@ -51,8 +52,24 @@ async function spellsCreation() {
 
 Hooks.once("ready", async function() {
 
+    let d = new Dialog({
+        title: "créer des compendium ?",
+        content: "<p>choississez les compendium à créer</p>",
+        buttons: {
+            one: {
+                icon: '<i class="fas fa-check"></i>',
+                label: "créer le compendium monstres",
+                callback: () => monsterCreation()
+            },
+            two: {
+                icon: '<i class="fas fa-check"></i>',
+                label: "créer le compendium sorts",
+                callback: () => spellCreation()
+            }
+        }
 
 
+    });
     //CONFIG.debug.hooks = true;
     console.log(`--------Heros et Dragons SRD Ready`);
     console.log(`
@@ -100,6 +117,11 @@ Hooks.once("ready", async function() {
     var logo = document.getElementById("logo");
     logo.setAttribute("src", "modules/srd-heros-et-dragons/img/logoHD.png");
     logo.setAttribute("title", "clickez pour créer les compendiums");
+    logo.addEventListener("click", function() {
+
+        d.render(true);
+
+    });
 
 
     /*
