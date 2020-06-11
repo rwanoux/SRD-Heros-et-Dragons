2c9d7a131fb05a6232bfba825799ecc311427767
rwan <erwan_lemaire@yahoo.fr>
2020-06-11 11:15:46 +0200

    suppresion fichiers inutils

--- modules/preloadTemplates.js
+++
@@ -1,8 +0,0 @@
-export const preloadTemplates = async function() {
-    const templatePaths = [
-        // Add paths to "modules/essai/templates"
-        "modules/srd-heros-et-dragons/templates/srdHDMenu.html"
-    ];
-
-    return loadTemplates(templatePaths);
-}
