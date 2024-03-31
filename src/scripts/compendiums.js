031f10abf2c337eef4120c5b72b5f8d5cbf022f3
p4535992 <tentimarco0@gmail.com>
2024-03-31 14:00:11 +0200

    Rewrite and upgrade the code for LevelDB and v11, also ecported the packs for better maintance (checkou the README)

--- src/scripts/compendiums.js
+++ src/scripts/compendiums.js
@@ -1,6 +1,6 @@
 import { MODULE_ID } from "../module";
 
-//---------------------compendium color---visibité des compendium H&D
+//---------------------compendium color---visibite des compendium H&D
 export function compendiumColor() {
     /*
     var comps = document.getElementsByClassName("pack-title");
@@ -17,14 +17,142 @@ export function compendiumColor() {
     */
     if (game.modules.get("compendium-themer")?.active) {
         game.modules.get("compendium-themer").api.addModule(MODULE_ID, [
-            // {
-            //     dataPack: `${MODULE_ID}.monsters`,
-            //     colorText: `#90EE90`,
-            //     iconText: `fa-solid fa-helmet-battle`,
-            //     bannerImage: `modules/${MODULE_ID}/assets/images/banner.webp`,
-            //     backgroundColorText: `#CD5C5C`,
-            // },
-            // <i class="fa-solid fa-pen-ruler"></i>
+            // ACTOR
+            {
+                dataPack: `${MODULE_ID}.HnD-dons`,
+                colorText: `#90EE90`,
+                iconText: `fa-solid fa-helmet-battle`,
+                bannerImage: `modules/${MODULE_ID}/assets/images/banner.webp`,
+                backgroundColorText: ``,
+            },
+            {
+                dataPack: `${MODULE_ID}.HnD-capacites-de-classes`,
+                colorText: `#90EE90`,
+                iconText: `fa-solid fa-helmet-battle`,
+                bannerImage: `modules/${MODULE_ID}/assets/images/banner.webp`,
+                backgroundColorText: ``,
+            },
+            {
+                dataPack: `${MODULE_ID}.HnD-grimoire`,
+                colorText: `#90EE90`,
+                iconText: `fa-solid fa-helmet-battle`,
+                bannerImage: `modules/${MODULE_ID}/assets/images/banner.webp`,
+                backgroundColorText: ``,
+            },
+            {
+                dataPack: `${MODULE_ID}.HnD-races`,
+                colorText: `#90EE90`,
+                iconText: `fa-solid fa-helmet-battle`,
+                bannerImage: `modules/${MODULE_ID}/assets/images/banner.webp`,
+                backgroundColorText: ``,
+            },
+            {
+                dataPack: `${MODULE_ID}.HnD-traits-raciaux`,
+                colorText: `#90EE90`,
+                iconText: `fa-solid fa-helmet-battle`,
+                bannerImage: `modules/${MODULE_ID}/assets/images/banner.webp`,
+                backgroundColorText: ``,
+            },
+            {
+                dataPack: `${MODULE_ID}.HnD-equipement-aventurier`,
+                colorText: `#90EE90`,
+                iconText: `fa-solid fa-helmet-battle`,
+                bannerImage: `modules/${MODULE_ID}/assets/images/banner.webp`,
+                backgroundColorText: ``,
+            },
+            {
+                dataPack: `${MODULE_ID}.HnD-historiques`,
+                colorText: `#90EE90`,
+                iconText: `fa-solid fa-helmet-battle`,
+                bannerImage: `modules/${MODULE_ID}/assets/images/banner.webp`,
+                backgroundColorText: ``,
+            },
+            {
+                dataPack: `${MODULE_ID}.HnD-Classes-specialisations`,
+                colorText: `#90EE90`,
+                iconText: `fa-solid fa-helmet-battle`,
+                bannerImage: `modules/${MODULE_ID}/assets/images/banner.webp`,
+                backgroundColorText: ``,
+            },
+            {
+                dataPack: `${MODULE_ID}.HnD-aptitudes-historiques`,
+                colorText: `#90EE90`,
+                iconText: `fa-solid fa-helmet-battle`,
+                bannerImage: `modules/${MODULE_ID}/assets/images/banner.webp`,
+                backgroundColorText: ``,
+            },
+            // ITEM
+            {
+                dataPack: `${MODULE_ID}.HnD-objets-magiques-et-tresors`,
+                colorText: `#90EE90`,
+                iconText: `fa-solid fa-sword`,
+                bannerImage: `modules/${MODULE_ID}/assets/images/banner.webp`,
+                backgroundColorText: ``,
+            },
+            {
+                dataPack: `${MODULE_ID}.HnD-poisons-et-maladies`,
+                colorText: `#90EE90`,
+                iconText: `fa-solid fa-sword`,
+                bannerImage: `modules/${MODULE_ID}/assets/images/banner.webp`,
+                backgroundColorText: ``,
+            },
+            // RULES
+            {
+                dataPack: `${MODULE_ID}.HnD-tables-de-tresors`,
+                colorText: `#90EE90`,
+                iconText: `fa-solid fa-pen-ruler`,
+                bannerImage: `modules/${MODULE_ID}/assets/images/banner.webp`,
+                backgroundColorText: ``,
+            },
+            {
+                dataPack: `${MODULE_ID}.HnD-conditions`,
+                colorText: `#90EE90`,
+                iconText: `fa-solid fa-pen-ruler`,
+                bannerImage: `modules/${MODULE_ID}/assets/images/banner.webp`,
+                backgroundColorText: ``,
+            },
+            {
+                dataPack: `${MODULE_ID}.HnD-bestaire`,
+                colorText: `#90EE90`,
+                iconText: `fa-solid fa-pen-ruler`,
+                bannerImage: `modules/${MODULE_ID}/assets/images/banner.webp`,
+                backgroundColorText: ``,
+            },
+            {
+                dataPack: `${MODULE_ID}.HnD-capacites-du-bestiaire`,
+                colorText: `#90EE90`,
+                iconText: `fa-solid fa-pen-ruler`,
+                bannerImage: `modules/${MODULE_ID}/assets/images/banner.webp`,
+                backgroundColorText: ``,
+            },
+            {
+                dataPack: `${MODULE_ID}.HnD-tables-de-rencontres-aleatoires`,
+                colorText: `#90EE90`,
+                iconText: `fa-solid fa-pen-ruler`,
+                bannerImage: `modules/${MODULE_ID}/assets/images/banner.webp`,
+                backgroundColorText: ``,
+            },
+            {
+                dataPack: `${MODULE_ID}.HnD-tables-d_evenements-aleatoires`,
+                colorText: `#90EE90`,
+                iconText: `fa-solid fa-pen-ruler`,
+                bannerImage: `modules/${MODULE_ID}/assets/images/banner.webp`,
+                backgroundColorText: ``,
+            },
+            {
+                dataPack: `${MODULE_ID}.HnD-folies`,
+                colorText: `#90EE90`,
+                iconText: `fa-solid fa-pen-ruler`,
+                bannerImage: `modules/${MODULE_ID}/assets/images/banner.webp`,
+                backgroundColorText: ``,
+            },
+            {
+                dataPack: `${MODULE_ID}.HnD-pieges`,
+                colorText: `#90EE90`,
+                iconText: `fa-solid fa-pen-ruler`,
+                bannerImage: `modules/${MODULE_ID}/assets/images/banner.webp`,
+                backgroundColorText: ``,
+            },
         ]);
     }
 }
